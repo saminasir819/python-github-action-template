085c15d5d69e32be033e0223a96bb8985c6a25e1
rickgoesrolling <93288507+saminasir819@users.noreply.github.com>
2024-10-03 23:11:22 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import requests
 discord_webhook_url = "https://discord.com/api/webhooks/1287762242300809277/oo7Lw_5xgLnX4nBMWHIQCql3xL_9JoMIpe7YKzX7NLl0Y_WQOXu51czoA9S37C1mxxjz"
 
 payload = {
-    "content": "This is a test message from the script.",
+    "content": "NIHAOOOOOOOOOOOOOOOOOOOOOO",
     "username": "Test Bot"
 }
 
