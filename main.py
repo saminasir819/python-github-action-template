e86424511d9d49e48321056b371505505b1908f2
rickgoesrolling <93288507+saminasir819@users.noreply.github.com>
2024-10-03 23:12:30 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import requests
 
-discord_webhook_url = "https://discord.com/api/webhooks/1287762242300809277/oo7Lw_5xgLnX4nBMWHIQCql3xL_9JoMIpe7YKzX7NLl0Y_WQOXu51czoA9S37C1mxxjz"
+discord_webhook_url = ""
 
 payload = {
     "content": "NIHAOOOOOOOOOOOOOOOOOOOOOO",
